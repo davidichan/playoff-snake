69fcb95e908ec5ac2b955ab6c3d8768313c414e7
davidichan <david.i.chan@gmail.com>
2017-01-15 21:40:29 -0700

    Modified ui text.

--- ui.R
+++ ui.R
@@ -1,6 +1,4 @@
-# Rely on the 'WorldPhones' dataset in the datasets
-# package (which generally comes preloaded).
-library(datasets)
+library(plotly)
 
 # Use a fluid Bootstrap layout
 fluidPage(    
@@ -14,10 +12,10 @@ fluidPage(
     # Define the sidebar with one input
     sidebarPanel(
       selectInput("chtype", "Chart type:", 
-                  choices=c("ANA", "ARI", "BOS", "BUF", "CAR", "CGY", "CHI", "CBJ", "COL", "DAL", "DET", "EDM", "FLA", "LAK", "MIN", "MTL", "NSH", "NJD", "NYI", "NYR", "OTT", "PHI", "PIT", "SJS", "STL", "TBL", "TOR", "VAN", "WSH", "WPG"),
+                  choices=c("ANA", "ARI", "BOS", "BUF", "CAR", "CBJ", "CGY", "CHI", "COL", "DAL", "DET", "EDM", "FLA", "LAK", "MIN", "MTL", "NSH", "NJD", "NYI", "NYR", "OTT", "PHI", "PIT", "SJS", "STL", "TBL", "TOR", "VAN", "WSH", "WPG"),
                   selected = "CGY"),                  
       hr(),
-      helpText("Plots how the Calgary Flames are doing in relation to the pace of a 96 point season."),
+      helpText("Plots how each team is doing in relation to the pace of a 96 point season."),
       br(),
       radioButtons("snakeType", "Snake type",
                    c("Traditional snake"="t", "Smart snake"="s")
@@ -26,9 +24,8 @@ fluidPage(
     
     # Create a spot for the barplot
     mainPanel(
-      plotlyOutput("phonePlot"),
-      helpText("Black - 96 point pace, red - 16-17 season, purple - 15-16 season, green - 14-15 season, (Smart snake only: gray - 96 point pace adjusted for opposition strength)"),
-      helpText("Playoff Snake idea adapted from ", a("Calgary Puck Forum", href="http://forum.calgarypuck.com/showthread.php?t=157390"))
+      plotlyOutput("SnakePlot"),
+      helpText("Thank you to the ", a("Calgary Puck Forum", href="http://forum.calgarypuck.com/showthread.php?t=157390"), " for the idea for the Playoff Snake.")
       #textOutput("phonePlot")  
     )
     
