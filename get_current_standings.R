8856fe07f3347a1c3b89538d49e39cf17913bc80
davidichan <david.i.chan@gmail.com>
2018-01-11 06:46:27 +0000

    Made mofidifications for 2017-18 season, removed strength of oponent feature

--- get_current_standings.R
+++ get_current_standings.R
@@ -5,9 +5,11 @@ library(gridExtra)
 
 rootp <- "data/"
 load(paste0(rootp,"NHL_standings_2016.RData"))
+load(paste0(rootp,"NHL_standings_2017.RData"))
 load(paste0(rootp,"cgy_points_hist.RData"))
 load(paste0(rootp,"season_records_2015.RData"))
 load(paste0(rootp,"season_records_2016.RData"))
+load(paste0(rootp,"season_records_2017.RData"))
 
 clean_results <- function(results.ls){
   cols <- ncol(results.ls[[1]]) + 1
@@ -29,37 +31,58 @@ clean_results <- function(results.ls){
   return(results.clean)
 }
 
-  seasons <- c("2017")
+#Specify the current season and teams to grab data for
+  seasons <- c("2018")
   #teams <- c("ANA", "CGY")
   teams <- c("ANA", "ARI", "BOS", "BUF", "CAR", "CGY", "CHI", "CBJ", "COL", "DAL", "DET", "EDM", "FLA", "LAK", "MIN", "MTL", "NSH", "NJD", "NYI", "NYR", "OTT", "PHI", "PIT", "SJS", "STL", "TBL", "TOR", "VAN", "WSH", "WPG")
   post_URL <- paste0("/",seasons,"_games.html")
-  season2017_records <- lapply(teams, function(x) readHTMLTable(paste0("http://www.hockey-reference.com/teams/",x,post_URL,""), header=T))
-  names(season2017_records) <- paste0(teams, seasons)
+  current_records <- lapply(teams, function(x) readHTMLTable(getURL(paste0("https://www.hockey-reference.com/teams/",x,post_URL,"")), header=T))
+  names(current_records) <- paste0(teams, seasons)
+
+#Clean up the table grabbed for each team
+  current.records.clean <- lapply(current_records, function(x) clean_results(x))
+
+##Get current points above the snake
+current.snake.diff <- lapply(current.records.clean, function(x) x$Points[!is.na(x$Points)] %>% last)
+current.snake.diff <- as.data.frame(unlist(current.snake.diff))
+names(current.snake.diff) <- "PointsDiff"
+
   
-  season2017.records.clean <- lapply(season2017_records, function(x) clean_results(x))
-  
+#Create new columns for each team
   for(i in seq_along(teams)){
-    season2017.records.clean[[i]]$P2015 <- season_records2015.clean[[i]]$Points
-    season2017.records.clean[[i]]$P2016 <- season_records2016.clean[[i]]$Points
-    season2017.records.clean[[i]]$Snake <- season2017.records.clean[[i]]$GP * (96/82.0)
+    #Add in a column for the number of points over 82 games for each of the preceding years, along with a linear line representing
+    #the 96 point pace
+    if(nrow(current.records.clean[[i]]) < 82){
+      current.records.clean[[i]] <- rbind(current.records.clean[[i]], list(82,NA,NA,NA,NA,NA,NA,NA,NA))
+    }
+    current.records.clean[[i]]$P2015 <- season_records2015.clean[[i]]$Points
+    current.records.clean[[i]]$P2016 <- season_records2016.clean[[i]]$Points
+    current.records.clean[[i]]$P2017 <- season2017.records.clean[[i]]$Points
+    current.records.clean[[i]]$Snake <- current.records.clean[[i]]$GP * (96/82.0)
     
-    season2017.records.clean[[i]]$P17_snake <- season2017.records.clean[[i]]$Points - season2017.records.clean[[i]]$Snake
-    season2017.records.clean[[i]]$P16_snake <- season2017.records.clean[[i]]$P2016 - season2017.records.clean[[i]]$Snake
-    season2017.records.clean[[i]]$P15_snake <- season2017.records.clean[[i]]$P2015 - season2017.records.clean[[i]]$Snake
+    #Calculate the point differential to the 96 point pace for each game
+    current.records.clean[[i]]$P18_snake <- current.records.clean[[i]]$Points - current.records.clean[[i]]$Snake
+    current.records.clean[[i]]$P17_snake <- current.records.clean[[i]]$P2017 - current.records.clean[[i]]$Snake
+    current.records.clean[[i]]$P16_snake <- current.records.clean[[i]]$P2016 - current.records.clean[[i]]$Snake
+    current.records.clean[[i]]$P15_snake <- current.records.clean[[i]]$P2015 - current.records.clean[[i]]$Snake
     
-    season2017.records.clean[[i]] <- join(season2017.records.clean[[i]], points16, by = "Opponent")
-    colnames(season2017.records.clean[[i]])[colnames(season2017.records.clean[[i]]) == "PTS"] <- "Pts_ssnake16"
-    
+    ### take out adjusted snake for now     
+    #Add in a column to show how many points each opponent got in the preceding season 
+    ##current.records.clean[[i]] <- join(current.records.clean[[i]], points16, by = "Opponent")
+    ##colnames(current.records.clean[[i]])[colnames(current.records.clean[[i]]) == "PTS"] <- "Pts_ssnake16"
+   
     #For the adjusted snake, taking into account the strength of the opponents
-    #Game against worst team is worth 96/82 + 1 points, against best team, is worth 96/82 + 1 pts
+    #Game against worst team is worth 96/82 + 1 points, against best team, is worth 96/82 - 1 pts
     #Using a range of +/- 27 points as the min and max points
-    season2017.records.clean[[i]]$SS17 <- season2017.records.clean[[i]]$Pts_ssnake16/(-27) + 4.726
+    ##season2017.records.clean[[i]]$SS17 <- season2017.records.clean[[i]]$Pts_ssnake16/(-27) + 4.726
+    
     #Using this method, the Flames faced disproportionately poor opposition and sum of snake points would
     #have been >96 points. Therefore, sum up the points, divide and readjust each point total to sum to 96. 
-    int_total <- cumsum(season2017.records.clean[[i]]$SS17)[82]
-    season2017.records.clean[[i]]$SS17 <- season2017.records.clean[[i]]$SS17 / int_total * 96
-    season2017.records.clean[[i]]$deltaSS17 <- season2017.records.clean[[i]]$Points - cumsum(season2017.records.clean[[i]]$SS17)
+    ##int_total <- cumsum(season2017.records.clean[[i]]$SS17)[82]
+    ##season2017.records.clean[[i]]$SS17 <- season2017.records.clean[[i]]$SS17 / int_total * 96
+    ##season2017.records.clean[[i]]$deltaSS17 <- season2017.records.clean[[i]]$Points - cumsum(season2017.records.clean[[i]]$SS17)
   }
   
-  names(season2017.records.clean) <- paste0(teams, seasons)
-  save(season2017.records.clean, file=paste0(rootp,"current_records_2017.RData"))
+  names(current.records.clean) <- paste0(teams, seasons)
+  save(current.records.clean, file=paste0(rootp,"current_records.RData"))
+  
--- server.R
+++ server.R
@@ -10,7 +10,8 @@ load(paste0(rootp,"NHL_standings_2016.RData"))
 load(paste0(rootp,"cgy_points_hist.RData"))
 load(paste0(rootp,"season_records_2015.RData"))
 load(paste0(rootp,"season_records_2016.RData"))
-load(paste0(rootp,"current_records_2017.RData"))
+load(paste0(rootp,"season_records_2017.RData"))
+load(paste0(rootp,"current_records.RData"))
 
 clean_results <- function(results.ls){
   cols <- ncol(results.ls[[1]]) + 1
@@ -43,22 +44,33 @@ function(input, output) {
   
   output$SnakePlot <- renderPlotly({ 
     if(input$snakeType == "t"){
-      t_name <- paste0(input$chtype, "2017")
+      t_name <- paste0(input$chtype, "2018")
       print(t_name)
       #trad <- ggplot(data= season2017.records.clean[[t_name]], aes(x=GP, y=Points)) + geom_line(colour="red", size=1) + geom_line(aes(x=GP, y=P2016), colour="purple4", size=0.75) + geom_line(aes(x=GP, y=P2015), colour="forestgreen", size=0.75) + geom_line(aes(x=GP, y=Snake), size=0.75) + snake_labs + snake_lims
-      trad <- plot_ly(data = season2017.records.clean[[t_name]], x = ~GP, y = ~Points, mode = "lines", name = '2016-17 Season', type = 'scatter', mode = 'lines', line = list(color = 'red', width = 3)) %>% add_trace(y = ~P2016, name = '2015-2016 Season', line = list(color = 'purple', width = 2)) %>% add_trace(y = ~P2015, name = '2015-16 Season', line = list(color = 'green', width = 2)) %>% add_trace(y = ~Snake, name = '96 Point pace', line = list(color = 'black', width = 2), showlegend = F) %>%
+      trad <- plot_ly(data = current.records.clean[[t_name]], x = ~GP, 
+        y = ~Points, mode = "lines", name = '2017-18 Season', type = 'scatter', mode = 'lines', line = list(color = 'red', width = 3)) %>% 
+        add_trace(y = ~P2017, name = '2016-17 Season', line = list(color = 'blue', width = 2)) %>% 
+        add_trace(y = ~P2016, name = '2015-16 Season', line = list(color = 'purple', width = 2), visible = "legendonly") %>% 
+        add_trace(y = ~P2015, name = '2014-15 Season', line = list(color = 'green', width = 2), visible = "legendonly") %>% 
+        add_trace(y = ~Snake, name = '96 Point pace', line = list(color = 'black', width = 2), showlegend = F) %>%
         layout(xaxis = list(title = "Games Played"), yaxis = list (title = "Points", range = c(0,110)))
-      #trad_diff <- ggplot(data= season2017.records.clean[[t_name]], aes(x=GP, y=P17_snake)) + geom_line(colour="red", size=1) + geom_line(aes(x=GP, y=P16_snake), colour="purple4", size=0.75) + geom_line(aes(x=GP, y=P15_snake), colour="forestgreen", size=0.75) + diff_labs + geom_hline(yintercept = 0, size=0.75) + diff_lims
-      trad_diff <- plot_ly(data = season2017.records.clean[[t_name]], x = ~GP, y = ~P17_snake, mode = "lines", name = '2016-17 differential', type = 'scatter', mode = 'lines', line = list(color = 'red', width = 3)) %>% add_trace(y = ~P16_snake, name = '2015-16 differential', line = list(color = 'purple', width = 2)) %>% add_trace(y = ~P15_snake, name = '2014-15 differential', line = list(color = 'green', width = 2)) %>% layout(xaxis = list(title = "Games Played"), yaxis = list (title = "Point diff. to snake", range = c(-15,15))) %>% add_trace(x = c(0, 82), y = c(0, 0), line=list(color='black', width=2), name = 'Snake', showlegend = F)
+      
+      trad_diff <- plot_ly(data = current.records.clean[[t_name]], x = ~GP, 
+        y = ~P18_snake, mode = "lines", name = '2017-18 differential', type = 'scatter', mode = 'lines', line = list(color = 'red', width = 3)) %>% 
+        add_trace(y = ~P17_snake, name = '2016-17 differential', line = list(color = 'blue', width = 2)) %>% 
+        add_trace(y = ~P16_snake, name = '2015-16 differential', line = list(color = 'purple', width = 2), visible = "legendonly") %>% 
+        add_trace(y = ~P15_snake, name = '2014-15 differential', line = list(color = 'green', width = 2), visible = "legendonly") %>% 
+        layout(xaxis = list(title = "Games Played"), yaxis = list (title = "Point diff. to snake", range = c(-15,15))) %>% 
+        add_trace(x = c(0, 82), y = c(0, 0), line=list(color='black', width=2), name = 'Snake', showlegend = F)
       subplot(trad, trad_diff, nrows = 2, shareX = T, titleX = T, titleY = T)
     } else if(input$snakeType == "s"){
       t_name <- paste0(input$chtype, "2017")
-      #smart_snake <- ggplot(data= season2017.records.clean[[t_name]], aes(x=GP, y=Points)) + geom_line(colour="red", size=1) + geom_line(aes(x=GP, y=P2016), colour="purple4", size=0.75) + geom_line(aes(x=GP, y=P2015), colour="forestgreen", size=0.75) + geom_line(aes(x=GP, y=cumsum(SS17)), colour="gray", size=0.75) + geom_line(aes(x=GP, y=Snake), colour="black", size=0.75) + snake_labs + snake_lims
-      smart_snake <- plot_ly(data = season2017.records.clean[[t_name]], x = ~GP, y = ~Points, mode = "lines", name = '2016-17 Season', type = 'scatter', mode = 'lines', line = list(color = 'red', width = 3)) %>% add_trace(y = ~P2016, name = '2015-2016 Season', line = list(color = 'purple', width = 2)) %>% add_trace(y = ~P2015, name = '2015-16 Season', line = list(color = 'green', width = 2)) %>% add_trace(y = ~Snake, name = '96 Point pace', line = list(color = 'black', width = 2), showlegend = F) %>%
-        layout(xaxis = list(title = "Games Played"), yaxis = list (title = "Points", range = c(0,110))) %>% add_trace(y = ~cumsum(SS17), name = 'Smart snake', line = list(color = 'black', width = 2, dash = 'dot'))
-      #smart_diff <- ggplot(data= season2017.records.clean[[t_name]], aes(x=GP, y=deltaSS17)) + geom_line(colour="red", size=1) + diff_labs + geom_hline(yintercept = 0, size=0.75) + diff_lims #+ geom_line(aes(x=GP, y=P16_snake), colour="purple4", size=0.75) + geom_line(aes(x=GP, y=P15_snake), colour="forestgreen", size=0.75) 
-      smart_diff <- plot_ly(data = season2017.records.clean[[t_name]], x = ~GP, y = ~deltaSS17, mode = "lines", name = '2016-17 Season', type = 'scatter', mode = 'lines', line = list(color = 'red', width = 3)) %>% layout(xaxis = list(title = "Games Played"), yaxis = list (title = "Point diff. to snake", range = c(-15,15)))  %>% add_trace(x = c(0, 82), y = c(0, 0), line=list(color='black', width=2), name = 'Snake', showlegend = F)
-      subplot(smart_snake, smart_diff, nrows = 2, shareX = T, titleX = T, titleY = T)
+
+#      smart_snake <- plot_ly(data = season2017.records.clean[[t_name]], x = ~GP, y = ~Points, mode = "lines", name = '2016-17 Season', type = 'scatter', mode = 'lines', line = list(color = 'red', width = 3)) %>% add_trace(y = ~P2016, name = '2015-2016 Season', line = list(color = 'purple', width = 2)) %>% add_trace(y = ~P2015, name = '2015-16 Season', line = list(color = 'green', width = 2)) %>% add_trace(y = ~Snake, name = '96 Point pace', line = list(color = 'black', width = 2), showlegend = F) %>%
+#        layout(xaxis = list(title = "Games Played"), yaxis = list (title = "Points", range = c(0,110))) %>% add_trace(y = ~cumsum(SS17), name = 'Smart snake', line = list(color = 'black', width = 2, dash = 'dot'))
+
+#      smart_diff <- plot_ly(data = season2017.records.clean[[t_name]], x = ~GP, y = ~deltaSS17, mode = "lines", name = '2016-17 Season', type = 'scatter', mode = 'lines', line = list(color = 'red', width = 3)) %>% layout(xaxis = list(title = "Games Played"), yaxis = list (title = "Point diff. to snake", range = c(-15,15)))  %>% add_trace(x = c(0, 82), y = c(0, 0), line=list(color='black', width=2), name = 'Snake', showlegend = F)
+#      subplot(smart_snake, smart_diff, nrows = 2, shareX = T, titleX = T, titleY = T)
     }
   })
 }
